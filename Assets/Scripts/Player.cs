15623be730ba092a7b80994702a92b3e3f9fe5f9
etiennebp <etiennebp@gmail.com>
2017-02-24 21:07:00 -0500

    Basic standing and running animations

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	private Animator animator;
+	private SpriteRenderer renderer;
+
+	void Start () {
+		animator = GetComponent<Animator>();
+		renderer = GetComponent<SpriteRenderer>();
+	}
+
+	void Update () {
+		int directionX = 0;
+		if (Input.GetKey(KeyCode.A)) directionX--;
+		if (Input.GetKey(KeyCode.D)) directionX++;
+
+		if (directionX != 0) {
+			renderer.transform.localScale = new Vector3(directionX, 1);
+			animator.SetBool("running", true);
+		}
+		else {
+			animator.SetBool("running", false);
+		}
+	}
+}
