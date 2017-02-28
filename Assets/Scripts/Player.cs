f81c881bab34835482950b04e0a3673baf869636
etiennebp <etiennebp@gmail.com>
2017-02-27 21:52:58 -0500

    2D player movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,10 +6,14 @@ public class Player : MonoBehaviour {
 
 	private Animator animator;
 	private SpriteRenderer renderer;
+	private Rigidbody2D body;
+
+	private float speed = 8;
 
 	void Start () {
 		animator = GetComponent<Animator>();
 		renderer = GetComponent<SpriteRenderer>();
+		body = GetComponent<Rigidbody2D>();
 	}
 
 	void Update () {
@@ -17,12 +21,26 @@ public class Player : MonoBehaviour {
 		if (Input.GetKey(KeyCode.A)) directionX--;
 		if (Input.GetKey(KeyCode.D)) directionX++;
 
+		int directionY = 0;
+		if (Input.GetKey(KeyCode.W)) directionY++;
+		if (Input.GetKey(KeyCode.S)) directionY--;
+
+		bool running = directionX != 0 || directionY != 0;
+		animator.SetBool("running", running);
+
+		float speedX = body.velocity.x;
+		float speedY = body.velocity.y;
+
 		if (directionX != 0) {
 			renderer.transform.localScale = new Vector3(directionX, 1);
-			animator.SetBool("running", true);
+			speedX = directionX * speed;
 		}
-		else {
-			animator.SetBool("running", false);
+
+		if (directionY != 0) {
+			speedY = directionY * speed * 0.75f;
 		}
+
+		body.velocity = new Vector2(speedX, speedY);
+
 	}
 }
