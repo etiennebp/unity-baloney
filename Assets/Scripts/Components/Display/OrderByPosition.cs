7595dbc0b036f6afd12357802e6273ca110e0fa6
etiennebp <etiennebp@gmail.com>
2017-03-01 20:35:34 -0500

    Order by position component

---
+++ Assets/Scripts/Components/Display/OrderByPosition.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class OrderByPosition : MonoBehaviour {
+
+	private SpriteRenderer spriteRenderer;
+
+	void Awake() {
+		spriteRenderer = GetComponent<SpriteRenderer>();
+	}
+
+	void LateUpdate() {
+		spriteRenderer.sortingOrder = -(int)Mathf.Floor(transform.position.y * 10);
+	}
+}
