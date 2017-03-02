3c9fc71235710b5b84ba70fe0dcd115ab58adc78
etiennebp <etiennebp@gmail.com>
2017-03-01 21:30:38 -0500

    Random wandering component

---
+++ Assets/Scripts/Components/Input/InputWander.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(DudeController))]
+public class InputWander : MonoBehaviour {
+
+	private DudeController controller;
+
+	private float direction = Random.Range(0f, 360f);
+
+	void Awake() {
+		controller = GetComponent<DudeController>();
+	}
+
+	void Update () {
+		direction += Random.Range(-20f, 20f);
+		direction = (direction + 360) % 360;
+		controller.input = new Vector2(Mathf.Cos(direction * Mathf.Deg2Rad), Mathf.Sin(direction * Mathf.Deg2Rad));
+	}
+}
