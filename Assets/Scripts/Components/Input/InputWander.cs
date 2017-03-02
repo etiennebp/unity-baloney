888e8815cb5854b0452c0b9cab67384f7c37dd20
etiennebp <etiennebp@gmail.com>
2017-03-01 21:36:31 -0500

    More random movement

--- Assets/Scripts/Components/Input/InputWander.cs
+++ Assets/Scripts/Components/Input/InputWander.cs
@@ -9,13 +9,41 @@ public class InputWander : MonoBehaviour {
 
 	private float direction = Random.Range(0f, 360f);
 
+	private bool walk;
+	private float timeTillToggle;
+
 	void Awake() {
 		controller = GetComponent<DudeController>();
+
+		walk = Random.value >= 0.5;
+		SetNextAction();
 	}
 
 	void Update () {
+		timeTillToggle -= Time.deltaTime;
+		if (timeTillToggle <= 0) {
+			SetNextAction();
+		}
+
+		if (walk) {
+			WalkLikeARetard();
+		}
+
+	}
+
+	private void SetNextAction() {
+		timeTillToggle = Random.Range(0.5f, 2f);
+		walk = !walk;
+
+		// reset
+		controller.input = new Vector2();
+		direction = Random.Range(0f, 360f);
+	}
+
+	private void WalkLikeARetard() {
 		direction += Random.Range(-20f, 20f);
 		direction = (direction + 360) % 360;
-		controller.input = new Vector2(Mathf.Cos(direction * Mathf.Deg2Rad), Mathf.Sin(direction * Mathf.Deg2Rad));
+		float rad = direction * Mathf.Deg2Rad;
+		controller.input = new Vector2(Mathf.Cos(rad), Mathf.Sin(rad));
 	}
 }
